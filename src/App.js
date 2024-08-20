cefe3326be52e0f8deeea9b602db4619fe8c6a1b
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 20:23:34 +0300

    adding about page and creating a route for it

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import About from './pages/About';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/auth' element={<Auth/>}/>
+      <Route path='/about' element={<About/>}/>
     </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/About.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const About = () => {
+    return (<></>);
+}
+
+export default About;
