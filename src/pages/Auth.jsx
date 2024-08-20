c2485a1aaf79be9a424c2fe98be9736e29f816f6
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 16:39:29 +0300

    starting with auth page implementation

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./styles/Auth.css";
 import Header from "../components/Header";
+import woman from "../images/working-woman.png";
 const Auth = () => {
-    return (
-        <>
-        <Header />
-        <div className="Auth">
-           
+  return (
+    <>
+      <Header />
+      <div className="Auth">
+        <div className="Auth-left">
+          <img src={woman} className="Auth-img"></img>
         </div>
-        </>
-    );
+        <div className="Auth-right"></div>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
