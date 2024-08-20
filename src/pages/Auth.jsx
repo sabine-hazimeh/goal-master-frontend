d61e151a2610e232873c122e4b71e26bbcac26c6
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 16:33:22 +0300

    adding header component to auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles/Auth.css";
-
+import Header from "../components/Header";
 const Auth = () => {
     return (
+        <>
+        <Header />
         <div className="Auth">
            
         </div>
+        </>
     );
 };
 
