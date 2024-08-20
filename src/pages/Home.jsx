e2a50b197b96e6fa3258268defc3412e8b885090
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 12:42:22 +0300

    Adding the left part of home introduction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="home-intro">
+        <div className="intro-left">
+          <h1>
+            Reach Your
+            <br />
+            Goal Easily!
+          </h1>
+          <p>
+            Get personalized recommendations, daily journaling, and AI-driven
+            insights to stay motivated and track your progress every step of the
+            way. Start your journey towards success today!
+          </p>
+          <div className="intro-buttons">
+            <button>Get Started!</button>
+            <button>Free Consultation</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
