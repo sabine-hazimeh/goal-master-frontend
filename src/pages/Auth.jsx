1af6fa73071bd431561c40a7f609aeb62d7c766a
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 17:47:01 +0300

    adding inputs field for login

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,6 +16,21 @@ const Auth = () => {
               <button className="Auth-button">Login</button>
               <button className="Auth-button">Sign Up</button>
             </div>
+            <div className="Auth-inputs">
+              <label className="Auth-label">Email</label>
+              <input
+                className="Auth-input"
+                type="text"
+                placeholder="Enter Email"
+              />
+              <label className="Auth-label">Password</label>
+              <input
+                className="Auth-input"
+                type="text"
+                placeholder="Enter Password"
+              />
+              <button className="Auth-submit">Submit</button>
+            </div>
           </div>
         </div>
       </div>
