f4001d04782da2c763a0c4e32de00405c9566e56
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 20:30:36 +0300

    adding first part for the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import "./styles/About.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
+import man from "../images/working-man.png";
 const About = () => {
-    return (<>
-    <Header />
-    <Footer />
-    </>);
-}
+  return (
+    <>
+      <Header />
+      <div className="about-upper">
+        <div className="upper-left">
+          <img src={man} className="upper-img"></img>
+        </div>
+        <div className="upper-right">
+          <p className="upper-title">About Us</p>
+          <p className="upper-text">
+            Welcome to <b>GoalMaster</b>, your ultimate partner in achieving
+            your dreams and aspirations.
+            <br />
+            <br /> At <b>GoalMaster</b>, we believe that every individual has
+            the potential to reach their goals, whether they are financial,
+            personal, or professional.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default About;
