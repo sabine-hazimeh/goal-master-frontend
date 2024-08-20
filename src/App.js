e13a49a05d8c620f4a44ca38da32ba847b60b710
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 16:31:21 +0300

    adding route for auth page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Auth from './pages/Auth';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/auth' element={<Auth/>}/>
     </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Auth.css";
+
+const Auth = () => {
+    return (
+        <div className="Auth">
+           
+        </div>
+    );
+};
+
+export default Auth;
