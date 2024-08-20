a210e7ff9d3f5e9e402ab6d32c7103a247d783e8
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 23:04:24 +0300

    adding journals form page

---
+++ src/pages/JournalsForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/JournalsForm.css";
+
+const JournalsForm = () => {
+    return (
+        <div className="journals-form">
+            
+        </div>
+    );
+};
+
+export default JournalsForm;
