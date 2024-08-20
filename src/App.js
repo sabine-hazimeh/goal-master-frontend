92f78de29dbd4caef3c28322af01eb678bde63f4
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 12:53:30 +0300

    adding routes for app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import './App.css';
-
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
