9053389ef9c58305a5a9170227acfb5a247eefa4
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 20:25:32 +0300

    adding header and footer components in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,10 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const About = () => {
-    return (<></>);
+    return (<>
+    <Header />
+    <Footer />
+    </>);
 }
 
 export default About;
