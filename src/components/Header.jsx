2fe99100594a9ec0ebff6bcf055dd3a23d57a104
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 14:34:17 +0300

    adding header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./styles/Header.css";
 import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
 const Header = () => {
     return (<>
     <header className="header">
         <div className="header-left">
         <img src={logo} className="header-logo"></img>
         <h1 className="header-title">Goal Master</h1>
+        </div>
+        <div className="header-right">
+            <Link to="#" className="header-links">Home</Link>
+            <Link to="#" className="header-links">Journal</Link>
+            <Link to="#" className="header-links">About</Link>
+            <Link to="#" className="header-links">Profile</Link>
+            <Link to="#" className="header-links">SignIn</Link>
         </div>
         
     </header>
