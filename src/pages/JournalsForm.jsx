fec13cc45084afdd209ab789384fb850b602f595
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-20 23:09:46 +0300

    adding image to journals page form

--- src/pages/JournalsForm.jsx
+++ src/pages/JournalsForm.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./styles/JournalsForm.css";
 import Header from "../components/Header";
+import woman from "../images/planning-woman.png";
 const JournalsForm = () => {
     return (
         <>
         <Header />
         <div className="journals-form">
-            
+            <div className="journals-left">
+                <img src={woman} className="journals-img"></img>
+            </div>
+            <div className="journals-right"></div>
         </div>
         </>
     );
