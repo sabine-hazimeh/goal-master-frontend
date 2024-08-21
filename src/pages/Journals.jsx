e715940b898e166f5f66d38ff80ed69739af276c
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-21 18:46:18 +0300

    adding button for adding new journals with it's style

--- src/pages/Journals.jsx
+++ src/pages/Journals.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
-
+import "./styles/Journals.css";
 function Journals() {
     return (<>
     <Header />
+    <div className="journals">
+    <p className="journals-text">Did you write in your journal today?</p>
+    <button className="journals-button">Add New Journal</button>
+    </div>
     </>);
 }
 
