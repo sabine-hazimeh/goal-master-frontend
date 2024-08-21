249335b41f29c9dc82bcb9ef9c92524cdf280347
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-21 20:56:44 +0300

    adding method to handle journal addition

--- src/pages/JournalsForm.jsx
+++ src/pages/JournalsForm.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/JournalsForm.css";
 import Header from "../components/Header";
 import woman from "../images/laptop.png";
 import FilledButton from "../components/FilledButton";
+import axios from "axios";
 const JournalsForm = () => {
+  const [mood, setMood] = useState("");
+  const [focus, setFocus] = useState("");
+  const [productivity, setProductivity] = useState("");
+  const [description, setDescription] = useState("");
+  const handleAddNewJournal = async () => {
+    const token = localStorage.getItem("Token");
+    if (!token) {
+        console.error("No authentication token found");
+        return;
+    }
+    try {
+        const response = await axios.post(
+            "http://localhost:8000/api/journal",
+            {
+                mood,
+                productivity,
+                description,
+                focus,
+                emotion_id: 1,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json" 
+                },
+            }
+        );
+        console.log("Journal added successfully", response.data);
+    } catch (error) {
+        console.error("Error adding journal:", error);
+    }
+};
   return (
     <>
       <Header />
       <div className="journals">
             <p className="journals-title">Today's Journal</p>
-            <p className="journals-text">Please fill out the form below to capture your thoughts and experiences for today.</p>
+            <p className="journals-form-text">Please fill out the form below to capture your thoughts and experiences for today.</p>
         </div>
       <div className="journals-form-container">
         <div className="journals-left">
