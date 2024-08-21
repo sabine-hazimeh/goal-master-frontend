c59b577a11c077d4314ef956d6079ac365cdb237
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-21 18:50:09 +0300

    adding navigation for add new journal button

--- src/pages/Journals.jsx
+++ src/pages/Journals.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Header from "../components/Header";
 import "./styles/Journals.css";
+import { useNavigate } from "react-router-dom";
+
 function Journals() {
-    return (<>
-    <Header />
-    <div className="journals">
-    <p className="journals-text">Did you write in your journal today?</p>
-    <button className="journals-button">Add New Journal</button>
-    </div>
-    </>);
+    const navigate = useNavigate();
+
+    const handleAddNewJournal = () => {
+        navigate("/journal-form");
+    };
+
+    return (
+        <>
+            <Header />
+            <div className="journals">
+                <p className="journals-text">Did you write in your journal today?</p>
+                <button className="journals-button" onClick={handleAddNewJournal}>
+                    Add New Journal
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default Journals;
