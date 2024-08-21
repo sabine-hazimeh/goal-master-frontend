772885917185ec51d2f9f630d2715b6ede83d345
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-21 18:56:06 +0300

    adding fetch journals for the authorized user

--- src/pages/Journals.jsx
+++ src/pages/Journals.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import "./styles/Journals.css";
 import { useNavigate } from "react-router-dom";
 
 function Journals() {
+    const [journals, setJournals] = useState([]);
     const navigate = useNavigate();
 
     const handleAddNewJournal = () => {
         navigate("/journal-form");
     };
-
+    useEffect(() => {
+        const fetchJournals = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/api/userJournals");
+                setJournals(response.data.journals); 
+            } catch (error) {
+                console.error("Error fetching journals:", error);
+            }
+        };
+        fetchJournals();
+    }, []);
     return (
         <>
             <Header />
@@ -18,6 +29,9 @@ function Journals() {
                 <button className="journals-button" onClick={handleAddNewJournal}>
                     Add New Journal
                 </button>
+                <div className="journals-container">
+                    
+                </div>
             </div>
         </>
     );
