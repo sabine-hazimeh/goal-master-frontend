2359cd11da2b00d4ed75bdaee4e95645bdf831bc
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-21 20:59:51 +0300

    connecting the form inputs to handleNewJournal method

--- src/pages/JournalsForm.jsx
+++ src/pages/JournalsForm.jsx
@@ -12,80 +12,87 @@ const JournalsForm = () => {
   const handleAddNewJournal = async () => {
     const token = localStorage.getItem("Token");
     if (!token) {
-        console.error("No authentication token found");
-        return;
+      console.error("No authentication token found");
+      return;
     }
     try {
-        const response = await axios.post(
-            "http://localhost:8000/api/journal",
-            {
-                mood,
-                productivity,
-                description,
-                focus,
-                emotion_id: 1,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json" 
-                },
-            }
-        );
-        console.log("Journal added successfully", response.data);
+      const response = await axios.post(
+        "http://localhost:8000/api/journal",
+        {
+          mood,
+          productivity,
+          description,
+          focus,
+          emotion_id: 1,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Journal added successfully", response.data);
     } catch (error) {
-        console.error("Error adding journal:", error);
+      console.error("Error adding journal:", error);
     }
-};
+  };
   return (
     <>
       <Header />
       <div className="journals">
-            <p className="journals-title">Today's Journal</p>
-            <p className="journals-form-text">Please fill out the form below to capture your thoughts and experiences for today.</p>
-        </div>
+        <p className="journals-title">Today's Journal</p>
+        <p className="journals-form-text">
+          Please fill out the form below to capture your thoughts and
+          experiences for today.
+        </p>
+      </div>
       <div className="journals-form-container">
         <div className="journals-left">
           <img src={woman} className="journals-img"></img>
         </div>
         <div className="journals-right">
-          <form className="journals-form">
-            <label className="journals-label">Dominant Emotions</label>
+          <form className="journals-form" onSubmit={handleAddNewJournal}>
+            {/* <label className="journals-label">Dominant Emotions</label>
             <input
               className="journals-input"
               type="text"
               placeholder="Enter Emotions"
-            />
+            /> */}
             <label className="journals-label">Overall mood</label>
             <input
               className="journals-input"
               type="text"
               placeholder="Enter Mood"
+              value={mood}
+              onChange={(e) => setMood(e.target.value)}
             />
             <label className="journals-label">Overall productivity</label>
             <input
               className="journals-input"
               type="text"
               placeholder="Enter Productivity"
+              value={productivity}
+              onChange={(e) => setProductivity(e.target.value)}
             />
             <label className="journals-label">Overall focus</label>
             <input
               className="journals-input"
               type="text"
               placeholder="Enter Focus"
+              value={focus}
+              onChange={(e) => setFocus(e.target.value)}
             />
             <label className="journals-label">Description</label>
             <textarea
               className="journals-input"
               type="text"
               placeholder="Enter Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
             <div className="journals-button-container">
-            <FilledButton
-              text="Submit"
-              onClick={() => {}}
-              className="journals-button"
-            />
+              <FilledButton text="Submit" className="journals-button" />
             </div>
           </form>
         </div>
