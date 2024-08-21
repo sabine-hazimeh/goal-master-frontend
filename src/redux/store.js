f946c746665855fbc79f99ed7eec1419c5ebf488
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-21 15:00:29 +0300

    adding users reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import { usersReducer } from "./usersSlice/slice";
 const store = configureStore({
     reducer: {
-
+        user: usersReducer,
     },
     middleware: (getDefaultMiddlware) => {
         return getDefaultMiddlware().concat(logger);
