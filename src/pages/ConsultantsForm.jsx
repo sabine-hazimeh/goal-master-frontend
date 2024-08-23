250e8a94dce8e7a45679107b89078bc6ab25ac49
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 20:52:59 +0300

    creating form for adding consultants details

--- src/pages/ConsultantsForm.jsx
+++ src/pages/ConsultantsForm.jsx
@@ -1,12 +1,61 @@
 import React from "react";
-import "./styles/ConsultantsForm.css";
+import "./styles/ConsultantForm.css";
 import Header from "../components/Header";
-
+import consultant from "../images/consultant.png";
+import FilledButton from "../components/FilledButton";
 function ConsultantsForm() {
   return (
     <>
       <Header />
-      <div className="ConsultantsForm"></div>
+      <div className="journals-form-container">
+        <div className="journals-left">
+          <img src={consultant} className="journals-img" alt="Laptop" />
+        </div>
+        <div className="journals-right">
+          <form className="journals-form">
+            <label className="journals-label">Name</label>
+            <input
+              className="journals-input"
+              type="text"
+              placeholder="Enter Name"
+            />
+            <label className="journals-label">Email</label>
+            <input
+              className="journals-input"
+              type="text"
+              placeholder="Enter Email"
+            />
+            <label className="journals-label">Password</label>
+            <input
+              className="journals-input"
+              type="text"
+              placeholder="Enter Productivity"
+            />
+            <label className="journals-label">Phone Number</label>
+            <input
+              className="journals-input"
+              type="text"
+              placeholder="Enter Number"
+            />
+            <label className="journals-label">Experience</label>
+            <input
+              className="journals-input"
+              type="text"
+              placeholder="Enter Experience"
+            />
+            <label className="journals-label">Description</label>
+            <textarea
+              className="journals-input"
+              placeholder="Enter Description"
+            />
+            <label className="Auth-label">Profile Photo</label>
+            <input type="file" />
+            <div className="journals-button-container">
+              <FilledButton text="Submit" className="journals-button" />
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
