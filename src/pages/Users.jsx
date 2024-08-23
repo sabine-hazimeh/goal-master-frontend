0ba9e2fbcbdee99156ea6f56d5107d311ee1bdbc
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 19:24:44 +0300

    adding fetch user method

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import "./styles/Users.css";
+import axios from "axios";
 
 function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const response = await axios.get("http://localhost:8000/api/users", {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("Token")}`,
+                        "Content-Type": "application/json",
+                    },
+                });
+                setUsers(response.data.users);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
     return (
         <div>
             <Header />
-            
         </div>
     );
 }
