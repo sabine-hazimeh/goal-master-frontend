dfd3af20c6ca105781adcfc4d6a294b19a09031f
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 14:30:21 +0300

    adding chats page

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/Chat.css";
+import Header from "../components/Header";
+
+function Chat() {
+  return (
+    <>
+      <Header />
+      <div className="Chat"></div>;
+    </>
+  );
+}
+
+export default Chat;
