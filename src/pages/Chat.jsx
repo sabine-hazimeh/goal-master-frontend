3436e3c96b404ac25203fbb266a372e08089d047
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 14:39:13 +0300

    adding message sending functionality

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./styles/Chat.css";
 import Header from "../components/Header";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Chat() {
     const { chatId } = useParams(); 
@@ -27,6 +29,25 @@ function Chat() {
     
         fetchMessages();
       }, [chatId]);
+      const handleSendMessage = async () => {
+        const token = localStorage.getItem("Token");
+        try {
+          const response = await axios.post(
+            `http://localhost:8000/api/message`,
+            { content: newMessage, chat_id: chatId },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          setMessages([...messages, response.data.message]);
+          setNewMessage("");
+        } catch (error) {
+          console.error("Error sending message:", error);
+        }
+      };
   return (
     <>
       <Header />
