b51d697ca5067c2f2dc7889935c2f9455ccb8d75
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 11:02:17 +0300

    adding fetch consultants method

--- src/pages/Consultants.jsx
+++ src/pages/Consultants.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Consultants.css";
+import axios from "axios";
 
 function Consultants() {
-    return (
-        <div className="Consultants">
-            
-        </div>
-    );
+  const [consultants, setConsultants] = useState([]);
+
+  useEffect(() => {
+    async function fetchConsultants() {
+      const token = localStorage.getItem("Token");
+      if (!token) {
+        console.error("No authentication token found");
+        return;
+      }
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/api/consultants`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setConsultants(response.data.consultants);
+      } catch (error) {
+        console.error("Error fetching consultants:", error);
+      }
+    }
+
+    fetchConsultants(); 
+  }, []);
+
+  return <div className="Consultants"></div>;
 }
 
 export default Consultants;
