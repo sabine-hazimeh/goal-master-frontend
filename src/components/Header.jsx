40fb971ac6d10b51eefb44310329115ece4087d5
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 19:07:41 +0300

    adding new header link for consultants

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,44 @@ import React from "react";
 import "./styles/Header.css";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Header = () => {
-    return (<>
-    <header className="header">
+  const user = useSelector((state) => state.user.user);
+  const isConsultant = user && user.role === "consultant";
+
+  return (
+    <>
+      <header className="header">
         <div className="header-left">
-        <img src={logo} className="header-logo"></img>
-        <h1 className="header-title">Goal Master</h1>
+          <img src={logo} className="header-logo" alt="Goal Master Logo" />
+          <h1 className="header-title">Goal Master</h1>
         </div>
         <div className="header-right">
-            <Link to="/" className="header-links">Home</Link>
-            <Link to="/journals" className="header-links">Journal</Link>
-            <Link to="/about" className="header-links">About</Link>
-            <Link to="#" className="header-links">Profile</Link>
-            <Link to="/auth" className="header-links">SignIn</Link>
+          <Link to="/" className="header-links">
+            Home
+          </Link>
+          <Link to="/journals" className="header-links">
+            Journal
+          </Link>
+          <Link to="/about" className="header-links">
+            About
+          </Link>
+          <Link to="#" className="header-links">
+            Profile
+          </Link>
+          {isConsultant && (
+            <Link to="/users" className="header-links">
+              Users
+            </Link>
+          )}
+          <Link to="/auth" className="header-links">
+            SignIn
+          </Link>
         </div>
-        
-    </header>
-    </>);
-}
+      </header>
+    </>
+  );
+};
 
 export default Header;
