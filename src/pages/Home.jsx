6e73358a01a8453bfb5337d2c3152a00e25e2024
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 15:30:17 +0300

    adding navigation to consultants page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,18 @@ import FilledButton from "../components/FilledButton";
 import EmptyButton from "../components/EmptyButton";
 import Header from "../components/Header";
 import Scan from "../images/face-scan.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBullseye, faBookOpen, faBrain } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBullseye,
+  faBookOpen,
+  faBrain,
+} from "@fortawesome/free-solid-svg-icons";
 import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
+
 const Home = () => {
   const navigate = useNavigate();
+
   return (
     <>
       <Header />
@@ -28,16 +34,19 @@ const Home = () => {
           </p>
           <div className="intro-buttons">
             <FilledButton text="Get Started!" onClick={() => {}} />
-            <EmptyButton text="Free Consultation" onClick={() => {}} />
+            <EmptyButton
+              text="Free Consultation"
+              onClick={() => navigate("/consultants")}
+            />
           </div>
         </div>
         <div className="intro-right">
-          <img src={GoalImage} className="intro-img"></img>
+          <img src={GoalImage} className="intro-img" alt="Goal Achievement" />
         </div>
       </div>
       <div className="face_scan">
         <div className="scan_right">
-          <img src={Scan} className="scan_img"></img>
+          <img src={Scan} className="scan_img" alt="Face Scan" />
         </div>
         <div className="scan_left">
           <p className="intro-title">
@@ -50,35 +59,37 @@ const Home = () => {
             real-time. Our system can recognize these emotions with high
             accuracy, allowing us to better understand how you're feeling.
           </p>
-          <FilledButton text="Try it now!"  onClick={() => navigate('/face-recognition')}/>
+          <FilledButton
+            text="Try it now!"
+            onClick={() => navigate("/face-recognition")}
+          />
         </div>
-        </div>
-        <div className="Features_container">
-          <p className="features_title">Why Goal Master?</p>
-          <div className="features">
-            <div className="feature">
-              <FontAwesomeIcon icon={faBullseye} className="feature_icon" />
-              <p>
-                Set and achieve your unique goals with personalized
-                recommendations.
-              </p>
-            </div>
-            <div className="feature">
-              <FontAwesomeIcon icon={faBookOpen} className="feature_icon" />
-              <p>
-                Capture your thoughts and experiences daily to reflect and grow
-                over time.
-              </p>
-            </div>
-            <div className="feature">
-              <FontAwesomeIcon icon={faBrain} className="feature_icon" />
-              <p>
-                AI-driven insights to stay motivated and track your progress
-                effectively.
-              </p>
-            </div>
+      </div>
+      <div className="Features_container">
+        <p className="features_title">Why Goal Master?</p>
+        <div className="features">
+          <div className="feature">
+            <FontAwesomeIcon icon={faBullseye} className="feature_icon" />
+            <p>
+              Set and achieve your unique goals with personalized
+              recommendations.
+            </p>
           </div>
-        
+          <div className="feature">
+            <FontAwesomeIcon icon={faBookOpen} className="feature_icon" />
+            <p>
+              Capture your thoughts and experiences daily to reflect and grow
+              over time.
+            </p>
+          </div>
+          <div className="feature">
+            <FontAwesomeIcon icon={faBrain} className="feature_icon" />
+            <p>
+              AI-driven insights to stay motivated and track your progress
+              effectively.
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
