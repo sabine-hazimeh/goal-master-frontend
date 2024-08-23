94a05db72149426d032e5bc58e4e15e4d2a2aa65
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 19:11:53 +0300

    adding users page

---
+++ src/pages/Users.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import "./styles/Users.css";
+
+function Users() {
+    return (
+        <div>
+            <Header />
+            
+        </div>
+    );
+}
+
+export default Users;
