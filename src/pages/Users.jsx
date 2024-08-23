88f7de6ca338d1e26b9d9f011fb1a1f91c0aa2b8
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 20:23:52 +0300

    adding method to handle the start of a chat

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,9 +3,10 @@ import Header from "../components/Header";
 import "./styles/Users.css";
 import axios from "axios";
 import Default from "../images/default-profile.jpeg";
+import { useNavigate } from "react-router-dom";
 function Users() {
   const [users, setUsers] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     async function fetchUsers() {
       try {
@@ -23,36 +24,53 @@ function Users() {
 
     fetchUsers();
   }, []);
+  const handleChatNow = async (userId) => {
+    const token = localStorage.getItem("Token");
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/api/chat`,
+        { consultant_id: userId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      navigate(`/chat/${response.data.chat.id}`);
+    } catch (error) {
+      console.error("Error starting chat:", error);
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="Users">
-      {users.map((user) => (
-        <div key={user._id} className="user-card">
+        {users.map((user) => (
+          <div key={user._id} className="user-card">
             <div className="user-details">
-          <img
-            src={user.profile_picture || Default}
-            className="Consultant-img"
-            alt="user profile"
-          />
-          <div className="user-info">
-          <h3>{user.name}</h3>
-          <p>{user.email}</p>
+              <img
+                src={user.profile_picture || Default}
+                className="Consultant-img"
+                alt="user profile"
+              />
+              <div className="user-info">
+                <h3>{user.name}</h3>
+                <p>{user.email}</p>
+              </div>
+            </div>
+
+            <div className="button-wrapper">
+              <button
+                className="Consultant-button"
+                onClick={() => handleChatNow(user.id)}
+              >
+                Chat now
+              </button>
+            </div>
           </div>
-          </div>
-          
-          <div className="button-wrapper">
-                  <button
-                    className="Consultant-button"
-                    // onClick={() => handleChatNow(user.id)}
-                  >
-                    Chat now
-                  </button>
-                </div>
-        </div>
-        
-      ))}
+        ))}
       </div>
     </>
   );
