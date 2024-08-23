e325a8f6fdcb69b1aab27710dde75a1ed6ebb02d
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-23 14:35:32 +0300

    Add message fetching to chat component

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Chat.css";
 import Header from "../components/Header";
 
 function Chat() {
+    const { chatId } = useParams(); 
+    const [messages, setMessages] = useState([]);
+  
+    useEffect(() => {
+        async function fetchMessages() {
+          const token = localStorage.getItem("Token");
+          try {
+            const response = await axios.get(
+              `http://localhost:8000/api/messages/${chatId}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+            setMessages(response.data.messages);
+          } catch (error) {
+            console.error("Error fetching messages:", error);
+          }
+        }
+    
+        fetchMessages();
+      }, [chatId]);
   return (
     <>
       <Header />
