90d328aa950c4c10d50335e907e45fcefa0c0234
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-24 15:38:43 +0300

    adding private route in app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import './App.css';
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Auth from './pages/Auth';
-import About from './pages/About';
-import FaceRecognition from './pages/FaceRecognition';
-import JournalsForm from './pages/JournalsForm';
-import Journals from './pages/Journals';
-import Consultants from './pages/Consultants';
-import Chat from './pages/Chat';
-import Users from './pages/Users';
-import ConsultantsForm from './pages/ConsultantsForm';
-import GoalsForm from './pages/GoalsForm';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import About from "./pages/About";
+import FaceRecognition from "./pages/FaceRecognition";
+import JournalsForm from "./pages/JournalsForm";
+import Journals from "./pages/Journals";
+import Consultants from "./pages/Consultants";
+import Chat from "./pages/Chat";
+import Users from "./pages/Users";
+import ConsultantsForm from "./pages/ConsultantsForm";
+import GoalsForm from "./pages/GoalsForm";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/auth' element={<Auth/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/face-recognition' element={<FaceRecognition/>}/>
-      <Route path='/journal-form' element={<JournalsForm/>}/>
-      <Route path='/journals' element={<Journals/>}/>
-      <Route path='/consultants' element={<Consultants/>}/>
-      <Route path="/chat/:chatId" element={<Chat />} />
-      <Route path='/users' element={<Users/>}/>
-      <Route path='/consultants-form' element={<ConsultantsForm/>}/>
-      <Route path='/goals-form' element={<GoalsForm/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/consultants-form" element={<ConsultantsForm />} />
+          <Route path="/goals-form" element={<GoalsForm />} />
+          <Route path="/journal-form" element={<JournalsForm />} />
+          <Route path="/journals" element={<Journals />} />
+          <Route path="/consultants" element={<Consultants />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/face-recognition" element={<FaceRecognition />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
