e27a7976e9b295b314a9e1b8bc8f317ac2a39b65
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-24 11:08:57 +0300

    adding forms for health and education goals

--- src/pages/GoalsForm.jsx
+++ src/pages/GoalsForm.jsx
@@ -10,17 +10,17 @@ function GoalsForm() {
         <div className="goals-form-left">
           <form>
             <label>
-              <input type="checkbox" name="categories" value="finance" />
+              <input type="checkbox" name="finance" value="finance" />
               Finance
             </label>
             <br />
             <label>
-              <input type="checkbox" name="categories" value="health" />
+              <input type="checkbox" name="health" value="health" />
               Health
             </label>
             <br />
             <label>
-              <input type="checkbox" name="categories" value="education" />
+              <input type="checkbox" name="education" value="education" />
               Education
             </label>
           </form>
@@ -49,6 +49,65 @@ function GoalsForm() {
             <br />
             <label>
               Target Date: <input type="date" name="target_date" />
+            </label>
+          </form>
+          <form className="goals-form">
+            <h3>Health Goals</h3>
+            <label>
+              Age: <input type="number" name="age" />
+            </label>
+            <br />
+            <label>
+              Gender:
+              <select name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </label>
+            <br />
+            <label>
+              Height: <input type="number" step="0.01" name="height" />
+            </label>
+            <br />
+            <label>
+              Current Weight:{" "}
+              <input type="number" step="0.01" name="current_weight" />
+            </label>
+            <br />
+            <label>
+              Desired Weight:{" "}
+              <input type="number" step="0.01" name="desired_weight" />
+            </label>
+            <br />
+            <label>
+              Medical Conditions:{" "}
+              <input type="text" name="medical_conditions" />
+            </label>
+            <br />
+            <label>
+              Time Horizon: <input type="date" name="time_horizon" />
+            </label>
+          </form>
+          <form className="education-form">
+            <h3>Education Goals</h3>
+            <label>
+              Goal: <input type="text" name="goal" />
+            </label>
+            <br />
+            <label>
+              Current Knowledge: <input type="text" name="current_knowledge" />
+            </label>
+            <br />
+            <label>
+              Available Days: <input type="number" name="available_days" />
+            </label>
+            <br />
+            <label>
+              Available Hours: <input type="number" name="available_hours" />
+            </label>
+            <br />
+            <label>
+              Time Horizon: <input type="date" name="time_horizon" />
             </label>
           </form>
         </div>
