a34669a023a3928f884d6dd9df0b77e5ace8d1d8
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-24 11:47:45 +0300

    adding styles for form input and label

--- src/pages/GoalsForm.jsx
+++ src/pages/GoalsForm.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./styles/GoalsForm.css";
 import Header from "../components/Header";
 import Goal from "../images/goal.png";
+
 function GoalsForm() {
   const [category, setCategory] = useState({
     finance: false,
     health: false,
     education: false,
   });
+
   function handleChange(e) {
     const { name, checked } = e.target;
     if (checked) {
@@ -24,6 +26,7 @@ function GoalsForm() {
       });
     }
   }
+
   return (
     <>
       <Header />
@@ -61,97 +64,132 @@ function GoalsForm() {
             </label>
           </form>
           <div className="goals-form-img">
-            <img src={Goal} className="goals-form-image"></img>
+            <img src={Goal} className="goals-form-image" alt="Goal" />
           </div>
         </div>
         <div className="goals-form-right">
           {category.finance && (
             <form className="goals-form">
               <h3>Finance Goals</h3>
-              <label>
-                Income <input type="number" name="income" />
-              </label>
-              <br />
-              <label>
-                Savings <input type="number" name="savings" />
-              </label>
-              <br />
-              <label>
-                Expenses <input type="number" name="expenses" />
-              </label>
-              <br />
-              <label>
-                Target <input type="number" name="target" />
-              </label>
-              <br />
-              <label>
-                Target Date <input type="date" name="target_date" />
-              </label>
+              <label className="goals-label">Income</label>
+              <input
+                type="number"
+                name="income"
+                className="goals-input"
+                placeholder="Enter Income"
+              />
+
+              <label className="goals-label">Savings</label>
+              <input
+                type="number"
+                name="savings"
+                className="goals-input"
+                placeholder="Enter Savings"
+              />
+
+              <label className="goals-label">Expenses</label>
+              <input
+                type="number"
+                name="expenses"
+                className="goals-input"
+                placeholder="Enter Expenses"
+              />
+
+              <label className="goals-label">Target</label>
+              <input
+                type="number"
+                name="target"
+                className="goals-input"
+                placeholder="Enter Target"
+              />
+
+              <label className="goals-label">Target Date</label>
+              <input type="date" name="target_date" className="goals-input" />
             </form>
           )}
           {category.health && (
             <form className="goals-form">
               <h3>Health Goals</h3>
-              <label>
-                Age <input type="number" name="age" />
-              </label>
-              <br />
-              <label>
-                Gender
-                <select name="gender">
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                </select>
-              </label>
-              <br />
-              <label>
-                Height <input type="number" step="0.01" name="height" />
-              </label>
-              <br />
-              <label>
-                Current Weight
-                <input type="number" step="0.01" name="current_weight" />
-              </label>
-              <br />
-              <label>
-                Desired Weight
-                <input type="number" step="0.01" name="desired_weight" />
-              </label>
-              <br />
-              <label>
-                Medical Conditions
-                <input type="text" name="medical_conditions" />
-              </label>
-              <br />
-              <label>
-                Time Horizon <input type="date" name="time_horizon" />
-              </label>
+              <label className="goals-label">Age</label>
+              <input
+                type="number"
+                name="age"
+                className="goals-input"
+                placeholder="Enter Age"
+              />
+              <label className="goals-label">Gender</label>
+              <select name="gender" className="goals-input">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+              <label className="goals-label">Height</label>
+              <input
+                type="number"
+                step="0.01"
+                name="height"
+                className="goals-input"
+                placeholder="Enter Height"
+              />
+              <label className="goals-label">Current Weight</label>
+              <input
+                type="number"
+                step="0.01"
+                name="current_weight"
+                className="goals-input"
+                placeholder="Enter Current Weight"
+              />
+              <label className="goals-label">Desired Weight</label>
+              <input
+                type="number"
+                step="0.01"
+                name="desired_weight"
+                className="goals-input"
+                placeholder="Enter Desired Weight"
+              />
+              <label className="goals-label">Medical Conditions</label>
+              <input
+                type="text"
+                name="medical_conditions"
+                className="goals-input"
+                placeholder="Enter Medical Conditions"
+              />
+              <label className="goals-label">Time Horizon</label>
+              <input type="date" name="time_horizon" className="goals-input" />
             </form>
           )}
           {category.education && (
             <form className="goals-form">
               <h3>Education Goals</h3>
-              <label>
-                Goal <input type="text" name="goal" />
-              </label>
-              <br />
-              <label>
-                Current Knowledge
-                <input type="text" name="current_knowledge" />
-              </label>
-              <br />
-              <label>
-                Available Days <input type="number" name="available_days" />
-              </label>
-              <br />
-              <label>
-                Available Hours <input type="number" name="available_hours" />
-              </label>
-              <br />
-              <label>
-                Time Horizon
-                <input type="date" name="time_horizon" />
-              </label>
+              <label className="goals-label">Goal</label>
+              <input
+                type="text"
+                name="goal"
+                className="goals-input"
+                placeholder="Enter Goal"
+              />
+              <label className="goals-label">Current Knowledge</label>
+              <input
+                type="text"
+                name="current_knowledge"
+                className="goals-input"
+                placeholder="Enter Current Knowledge"
+              />
+              <label className="goals-label">Available Days</label>
+              <input
+                type="number"
+                name="available_days"
+                className="goals-input"
+                placeholder="Enter Available Days"
+              />
+              <label className="goals-label">Available Hours</label>
+              <input
+                type="number"
+                name="available_hours"
+                className="goals-input"
+                placeholder="Enter Available Hours"
+              />
+              <label className="goals-label">Time Horizon</label>
+              <input type="date" name="time_horizon" className="goals-input" />
             </form>
           )}
         </div>
