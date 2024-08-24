cd50890b0e0d541f0e89053a5146ad4ee109661d
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-24 21:44:05 +0300

    adding content for profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./styles/Profile.css";
 import Header from "../components/Header";
-
+import Default from "../images/default-profile.jpeg";
+import FilledButton from "../components/FilledButton";
+import EmptyButton from "../components/EmptyButton";
 function Profile() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <div className="profile">
+        <p>Profile Picture</p>
+        <img src={Default} className="profile-img" alt="Profile" />
+        <FilledButton text="Change picture" />
+        <EmptyButton text="Delete picture" />
+        <form className="profile-form">
+          <label>Username</label>
+          <input type="text" />
+          <label>email</label>
+          <input type="email" />
+          <p>Change Password?</p>
+          <FilledButton text="Save Changes" />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
