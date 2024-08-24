99792c6f66e90eb15421d277175a243f15472588
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-24 11:34:56 +0300

    adding styles to the left part of goals form page

--- src/pages/GoalsForm.jsx
+++ src/pages/GoalsForm.jsx
@@ -27,7 +27,7 @@ function GoalsForm() {
   return (
     <>
       <Header />
-      <div className="goals-form">
+      <div className="goals-form-container">
         <div className="goals-form-left">
           <form>
             <label>
@@ -41,12 +41,22 @@ function GoalsForm() {
             </label>
             <br />
             <label>
-              <input type="checkbox" name="health" checked={category.health} onChange={handleChange} />
+              <input
+                type="checkbox"
+                name="health"
+                checked={category.health}
+                onChange={handleChange}
+              />
               Health
             </label>
             <br />
             <label>
-              <input type="checkbox" name="education" checked={category.education} onChange={handleChange} />
+              <input
+                type="checkbox"
+                name="education"
+                checked={category.education}
+                onChange={handleChange}
+              />
               Education
             </label>
           </form>
@@ -59,23 +69,23 @@ function GoalsForm() {
             <form className="goals-form">
               <h3>Finance Goals</h3>
               <label>
-                Income: <input type="number" name="income" />
+                Income <input type="number" name="income" />
               </label>
               <br />
               <label>
-                Savings: <input type="number" name="savings" />
+                Savings <input type="number" name="savings" />
               </label>
               <br />
               <label>
-                Expenses: <input type="number" name="expenses" />
+                Expenses <input type="number" name="expenses" />
               </label>
               <br />
               <label>
-                Target: <input type="number" name="target" />
+                Target <input type="number" name="target" />
               </label>
               <br />
               <label>
-                Target Date: <input type="date" name="target_date" />
+                Target Date <input type="date" name="target_date" />
               </label>
             </form>
           )}
@@ -83,11 +93,11 @@ function GoalsForm() {
             <form className="goals-form">
               <h3>Health Goals</h3>
               <label>
-                Age: <input type="number" name="age" />
+                Age <input type="number" name="age" />
               </label>
               <br />
               <label>
-                Gender:
+                Gender
                 <select name="gender">
                   <option value="male">Male</option>
                   <option value="female">Female</option>
@@ -95,51 +105,52 @@ function GoalsForm() {
               </label>
               <br />
               <label>
-                Height: <input type="number" step="0.01" name="height" />
+                Height <input type="number" step="0.01" name="height" />
               </label>
               <br />
               <label>
-                Current Weight:{" "}
+                Current Weight
                 <input type="number" step="0.01" name="current_weight" />
               </label>
               <br />
               <label>
-                Desired Weight:{" "}
+                Desired Weight
                 <input type="number" step="0.01" name="desired_weight" />
               </label>
               <br />
               <label>
-                Medical Conditions:{" "}
+                Medical Conditions
                 <input type="text" name="medical_conditions" />
               </label>
               <br />
               <label>
-                Time Horizon: <input type="date" name="time_horizon" />
+                Time Horizon <input type="date" name="time_horizon" />
               </label>
             </form>
           )}
           {category.education && (
-            <form className="education-form">
+            <form className="goals-form">
               <h3>Education Goals</h3>
               <label>
-                Goal: <input type="text" name="goal" />
+                Goal <input type="text" name="goal" />
               </label>
               <br />
               <label>
-                Current Knowledge:{" "}
+                Current Knowledge
                 <input type="text" name="current_knowledge" />
               </label>
               <br />
               <label>
-                Available Days: <input type="number" name="available_days" />
+                Available Days <input type="number" name="available_days" />
               </label>
               <br />
               <label>
-                Available Hours: <input type="number" name="available_hours" />
+                Available Hours <input type="number" name="available_hours" />
               </label>
               <br />
               <label>
-                Time Horizon: <input type="date" name="time_horizon" />
+                Time Horizon
+                <input type="date" name="time_horizon" />
               </label>
             </form>
           )}
