fd7ce3300677340a95f8811c2ab527a8b609c426
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-25 17:17:54 +0300

    adding function to fetch consultants

--- src/pages/AdminConsultants.jsx
+++ src/pages/AdminConsultants.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import "./styles/AdminConsultants.css";
-
+import axios from "axios";
 function AdminConsultants() {
+  const [consultants, setConsultants] = useState([]);
+  useEffect(() => {
+    async function fetchConsultants() {
+      const token = localStorage.getItem("Token");
+      if (!token) {
+        console.error("No authentication token found");
+        return;
+      }
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/api/consultants`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setConsultants(response.data[1]);
+      } catch (error) {
+        console.error("Error fetching consultants:", error);
+      }
+    }
+
+    fetchConsultants();
+  }, []);
   return (
     <div>
       <Header />
