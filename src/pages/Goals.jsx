6f12e27a47bba203ebf420a4656bc4cebb1b91f5
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-01 14:12:52 +0300

    adding useEffect to fetch the available goals

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Goals.css";
 import Header from "../components/Header";
-
+import axios from "axios";
 const Goals = () => {
+    const [goals, setGoals] = useState([]);
+    useEffect(() => {
+        const fetchGoals = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/api/goals",
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem("Token")}`,
+                            "Content-Type": "application/json",
+                        }
+                    }
+                );
+                setGoals(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchGoals();
+    },[]);
     return (
         <div>
             <Header />
