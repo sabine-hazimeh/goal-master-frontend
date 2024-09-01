3c037359b71cdbf4e3b6bc6cd0cc2c6360e9918c
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-01 13:41:53 +0300

    adding goal component

---
+++ src/pages/Goals.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/Goals.css";
+import Header from "../components/Header";
+
+const Goals = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default Goals;
