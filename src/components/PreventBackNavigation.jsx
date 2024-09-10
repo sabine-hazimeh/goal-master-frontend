5d3eba6242161f5e16c5417117175e4ca8780aaf
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-10 13:46:26 +0300

    updating private route to redirect to the desired page after login

--- src/components/PreventBackNavigation.jsx
+++ src/components/PreventBackNavigation.jsx
@@ -2,23 +2,20 @@ import { useEffect } from 'react';
 
 function PreventBackNavigation() {
   useEffect(() => {
-    // Push the current state into the history stack on mount
     window.history.pushState(null, document.title, window.location.href);
 
     const handlePopState = (event) => {
-      // Re-push the current state when the user tries to navigate back
       window.history.pushState(null, document.title, window.location.href);
     };
 
     window.addEventListener('popstate', handlePopState);
 
     return () => {
-      // Cleanup event listener on component unmount
       window.removeEventListener('popstate', handlePopState);
     };
   }, []);
 
-  return null; // This component does not render anything
+  return null; 
 }
 
 export default PreventBackNavigation;
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { isAuthenticated } from "../auth.js";
 
 const PrivateRoute = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/auth" />;
+  const location = useLocation();  
+  return isAuthenticated() ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/auth" state={{ from: location }} />
+  );
 };
 
 export default PrivateRoute;
