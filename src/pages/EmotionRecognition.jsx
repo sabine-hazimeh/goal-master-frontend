4e74ebb2c026fcf20ef3f04806a4a6b65566f98e
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-11 10:20:50 +0300

    adding function that performs real-time face detection and emotion recognition

--- src/pages/EmotionRecognition.jsx
+++ src/pages/EmotionRecognition.jsx
@@ -3,6 +3,7 @@ import * as faceapi from "face-api.js";
 
 const EmotionRecognition = () => {
   const videoRef = useRef(null);
+  const [emotion, setEmotion] = useState("");
   useEffect(() => {
     const loadModels = async () => {
       await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
@@ -17,6 +18,27 @@ const EmotionRecognition = () => {
         })
         .catch((err) => console.error("Error accessing webcam: ", err));
     };
+    const detectEmotions = async () => {
+      if (videoRef.current) {
+        const detections = await faceapi
+          .detectAllFaces(
+            videoRef.current,
+            new faceapi.TinyFaceDetectorOptions()
+          )
+          .withFaceExpressions();
+
+        if (detections.length > 0) {
+          const expressions = detections[0].expressions;
+          const dominantEmotion = Object.keys(expressions).reduce((a, b) =>
+            expressions[a] > expressions[b] ? a : b
+          );
+          setEmotion(dominantEmotion);
+        }
+      }
+    };
+
+    loadModels();
+    setInterval(detectEmotions, 1000);
   }, []);
 
   return <div></div>;
