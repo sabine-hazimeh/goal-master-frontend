8a6cf46b42e3d725d86ec585ae37fd57f362fe81
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-11 10:17:32 +0300

    adding method to access user's webcam

--- src/pages/EmotionRecognition.jsx
+++ src/pages/EmotionRecognition.jsx
@@ -2,11 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import * as faceapi from "face-api.js";
 
 const EmotionRecognition = () => {
+  const videoRef = useRef(null);
   useEffect(() => {
     const loadModels = async () => {
       await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
       await faceapi.nets.faceExpressionNet.loadFromUri("/models");
       startVideo();
+    };
+    const startVideo = () => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+        })
+        .catch((err) => console.error("Error accessing webcam: ", err));
     };
   }, []);
 
